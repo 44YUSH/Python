cd43e5e435641284bf5b62df2610eb10dfce8754
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-08 07:33:12 +0000

    .

---
+++ Python/AvgOfTwoFloatNo.py
@@ -0,0 +1,4 @@
+a = float(input("Enter the 1st no. : "))
+b = float(input("Enter the 2nd no. : "))
+avg = (a+b)/2
+print("Average is : ", avg)
---
+++ Python/TrueFalse.py
@@ -0,0 +1,3 @@
+a = int(input("1st no : "))
+b = int(input("2nd no : "))
+print(a >= b)
