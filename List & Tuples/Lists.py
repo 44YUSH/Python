079ffd45b9e60ab5f8d8eb6a348f1f25bb09d2d9
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-09 16:00:17 +0000

    .

---
+++ List & Tuples/Lists.py
@@ -0,0 +1,16 @@
+#Lists are MUTABLE or CHANGABLE.
+
+marks=[9, 8, 7, 4, 5]
+
+print(marks[2])
+
+
+student=["Radha", 97, "Barsana"]
+print(student[0], type(student[0]))
+print(student[1], type(student[1]))
+
+student[0]="Krishn"
+
+print(student)
+
+print(len(student))
