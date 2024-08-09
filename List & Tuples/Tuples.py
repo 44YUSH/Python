3ec9805433094e528108afdda9dd322c79e8fbf7
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-09 16:31:45 +0000

    .

---
+++ List & Tuples/Tuples.py
@@ -0,0 +1,13 @@
+# TUPLES are IMMUTABLE or can't be changed
+
+tup = (90, 80, 70, 60 ,50)
+print(type(tup))
+print(tup[1])
+
+# tup[0] = 10 is not possible
+
+tup1=()
+print(tup1())     # It's an Empty Tuple.
+
+tup2=(1,)
+print(tup2())
