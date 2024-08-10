0e2ff2d17d5b96da09a9d898536b59e4e90bac5d
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-10 16:59:05 +0000

    Baad Main Kar

--- List & Tuples/Q3.py
+++ List & Tuples/Q3.py
@@ -1,8 +1,21 @@
-t=tuple(input("Enter your tuple : "))
-print(t)
-print(t.count('a'))
+# t=tuple(input("Enter your tuple : "))
+# print(t)
+# print(t.count('a'))
 
-print(t)
-l=[]
-l.append(t)
+# print(t)
+# l=[]
+# l.append(t)
+# print(l)
+# l.sort()
+# print(l)
+
+l = []
+n = int(input("Enter the size of LIST : "))
+print("Enter the elements : ")
+for i in range (0,n):
+    ele=input()
+    l.append(ele)
+print("Your LIST is : ", l)
+
+l.sort()
 print(l)
