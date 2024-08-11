84e38d28dc680db629660342a1fc3e6d5d7003aa
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-11 16:27:57 +0000

    .

--- Dictionary & Sets/Methods_Sets.py
+++ Dictionary & Sets/Methods_Sets.py
@@ -15,3 +15,16 @@ s1=s.copy()
 s1.remove(3)
 s1.remove("Krishn")
 print(s1)
+
+# set.clear() empties the set.
+s2=s1.copy()
+
+s2.clear()
+print(s2)
+print(len(s2))
+
+# set.pop() removes a random value
+s3=s.copy()
+
+s3.pop()
+print(s3)
