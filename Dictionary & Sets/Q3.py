5ffa119e2535793161c9c175d64511059c9e6c7c
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-12 07:07:27 +0000

    .

---
+++ Dictionary & Sets/Q3.py
@@ -0,0 +1,13 @@
+# a = str(input("Enter 1st subject : "))
+# x = int(input("Enter marks of 1st subject : "))
+# b = str(input("Enter 2nd subject : "))
+# y = int(input("Enter marks of 2nd subject : "))
+# c = str(input("Enter 3rd subject : "))
+# z = int(input("Enter marks of 3rd subject : "))
+# result={
+#     a : x,
+#     b : y,
+#     c : z
+# }
+# print(result)
+
