255cf11242c50fddaba7e2fbbb3bd4f51ea8406e
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-12 17:40:43 +0000

    .

--- Dictionary & Sets/Q3.py
+++ Dictionary & Sets/Q3.py
@@ -11,3 +11,22 @@
 # }
 # print(result)
 
+result = {}
+print(result)
+
+a=input("Enter 1st subject : ")
+x=input("Enter MARKS of 1st sub : ")
+SUBJECT1={a : x}
+result.update(SUBJECT1)
+print(result)
+
+b = str(input("Enter 2nd subject : "))
+y = int(input("Enter marks of 2nd subject : "))
+SUBJECT2={b : y}
+result.update(SUBJECT2)
+print(result)
+
+c = str(input("Enter 3rd subject : "))
+z = int(input("Enter marks of 3rd subject : "))
+SUBJECT3={c : z}
+print(SUBJECT3)
