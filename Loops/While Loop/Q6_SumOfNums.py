5e6e23c63e47d7853bd4bcb54b4492d9c6ad6c9b
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-13 19:43:17 +0000

    .

--- Loops/While Loop/Q6_SumOfNums.py
+++ Loops/While Loop/Q6_SumOfNums.py
@@ -1,9 +1,16 @@
 # Find sum of 1+2+3+...+n.
 
+# While Loop :
 n = int(input("Enter the number : "))
 i=1
-sum=0
+sum1=0
 while i <= n:
-    sum+=i
+    sum1+=i
     i+=1
-print(sum)
+print("Using WHILE loop : ",sum1)
+
+# For Loop : 
+sum2=0
+for j in range(1, n+1):
+    sum2+=j
+print("Using FOR loop :  ",sum2)
