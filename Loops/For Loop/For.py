58598ed854c21936a5df699e2ba62e941f2f0857
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-13 07:05:03 +0000

    .

--- Loops/For Loop/For.py
+++ Loops/For Loop/For.py
@@ -1,6 +1,24 @@
 # Loops are used for Sequential traversal. For traversing list, string, tuples etc.
-list=[1,2,3]
-for el in list:
-    print(el)
+# LOOP1
+nums=[1,2,3,4,5]
+
+for val in nums:
+    print(val)
 else:
-    print("END")
+    print("-----")  # It's optional
+
+# LOOP2
+alphabets=["a","b","c","d","e"]
+
+for val in alphabets:
+    print(val)
+else:
+    print("-----")
+
+# LOOP3
+str="AAYUSH SITH"
+
+for char in str:
+    print(char)
+else:
+    print("-----")
