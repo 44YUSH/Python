61d471484914485893cd5a93dc11b4337ca0392d
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-13 19:18:17 +0000

    .

---
+++ Loops/For Loop/Pass_Statement.py
@@ -0,0 +1,7 @@
+# Pass is a null statement that does nothing.
+# It is used as a placeholder for future code.
+
+for el in range(10):
+    pass
+
+print("Now, we can print without getting" ,"\nany errors from the loop.")
