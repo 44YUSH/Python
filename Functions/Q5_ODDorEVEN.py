d9b664ff82ef570f11cc3f28d5cab5552e5c052c
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-14 20:05:41 +0000

    .

---
+++ Functions/Q5_ODDorEVEN.py
@@ -0,0 +1,11 @@
+# Find if the given num is ODD or EVEN.
+
+n=int(input("Enter the num : "))
+
+def OddEven(num):
+    if(num%2==0):
+        print("EVEN")
+    else:
+        print("ODD")
+
+OddEven(n)
