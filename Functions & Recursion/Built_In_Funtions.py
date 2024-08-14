b0de51e4a5bb09842779ff8f26eebc2ea3f914c3
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-14 18:45:35 +0000

    .

---
+++ Functions & Recursion/Built_In_Funtions.py
@@ -0,0 +1,10 @@
+# Functions in Python.
+# There are two types of functions in python.
+#     i. Built-In Functions :
+#                * print()
+#                * len()
+#                * type()
+#                * range(), etc.
+#    ii. User defined Functions : 
+#                * These functions are designed
+#                  and created by the users.
