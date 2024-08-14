15fe8d769b6b0fc0c7adc09ca75b2fbdbd6c1315
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-14 20:28:45 +0000

    .

--- Recursions/Recursion.py
+++ Recursions/Recursion.py
@@ -1,10 +1,10 @@
 # When a function calls itself repeatedly.     |       |
                                         #      +-------+
-def show(n):                            #      |   1   |
-    if(n==0):                           #      +-------+
-        return                          #      |   2   |
-    print(n)                            #      +-------+
-    show(n-1)                           #      |   3   |
-                                        #      +-------+
+def show(n):                            #      |   1   |  ----\   /-->  3
+    if(n==0):                           #      +-------+       \ /
+        return                          #      |   2   |  ------X---->  2
+    print(n)                            #      +-------+       / \
+    show(n-1)                           #      |   3   |  ----/   \-->  1
+                                        #      +-------+  
                                         #       L I F O
 show(3)                   
