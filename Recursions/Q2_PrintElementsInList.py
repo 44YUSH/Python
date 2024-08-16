cea9d92c81dc19604fc693997df0b577b5814d7c
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-16 15:48:39 +0000

    .

--- Recursions/Q2_PrintElementsInList.py
+++ Recursions/Q2_PrintElementsInList.py
@@ -1,5 +1,11 @@
 # Write a recursive function to print all elements in a list.
 # { HINT : Use LIST & INDEX as parameters. }
 
-def PrintElement(list, idx):
-    
+def PrintElement(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    PrintElement(list, idx+1)
+
+alphabets=["a","b","c","d"]
+PrintElement(alphabets)
