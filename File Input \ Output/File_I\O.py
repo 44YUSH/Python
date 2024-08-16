035bf589e1c4ae0ea9665da31a98f4046d276bd3
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-08-16 15:57:53 +0000

    .

---
+++ File Input \ Output/File_I\O.py
@@ -0,0 +1,3 @@
+# Python can be used to perform operations on a file. (read & write data).
+# Types of all files :  i.) Text Files - .txt , .docx , .log , etc
+#                      ii.) Binary Files - .mp4 , .mov , .png , .jepg , etc.
